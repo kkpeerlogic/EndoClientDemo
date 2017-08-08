5ced9086ead66bb3d205b8cb52b5fb61881f7bee
Peerlogic Systems <kanhaiya@peerlogicsys.com>
2017-08-08 16:25:11 +0530

    Added

---
+++ Example/EndoClientDemo-tvOS/EndoClientDemo-tvOS.h
@@ -0,0 +1,19 @@
+//
+//  EndoClientDemo-tvOS.h
+//  EndoClientDemo-tvOS
+//
+//  Created by Peerlogic Systems on 08/08/17.
+//  Copyright © 2017 kkpeerlogic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EndoClientDemo-tvOS.
+FOUNDATION_EXPORT double EndoClientDemo_tvOSVersionNumber;
+
+//! Project version string for EndoClientDemo-tvOS.
+FOUNDATION_EXPORT const unsigned char EndoClientDemo_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EndoClientDemo_tvOS/PublicHeader.h>
+
+
