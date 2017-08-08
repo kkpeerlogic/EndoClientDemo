29ef74b42d2c29cd064bf42d117b7cf44c4f16dd
Peerlogic Systems <kanhaiya@peerlogicsys.com>
2017-08-08 16:32:46 +0530

    Enable tvOS

---
+++ Example/Pods/EndoTVClient/EndoTVClient.h
@@ -0,0 +1,20 @@
+//
+//  EndoTVClient.h
+//  EndoTVClient
+//
+//  Created by Peerlogic Systems on 08/08/17.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EndoTVClient.
+FOUNDATION_EXPORT double EndoTVClientVersionNumber;
+
+//! Project version string for EndoTVClient.
+FOUNDATION_EXPORT const unsigned char EndoTVClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EndoTVClient/PublicHeader.h>
+
+
+#import "EndoAPI.h"
